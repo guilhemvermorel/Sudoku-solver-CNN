623100e98230de276d0de5cd1aa81c6bdf41b9b2
Guilhem VERMOREL <83694424+guilhemvermorel@users.noreply.github.com>
2022-12-16 17:12:48 -0500

    Create set_definition.py
    first commit

---
+++ set_definition.py
@@ -0,0 +1,59 @@
+import torch
+from torch.utils.data import DataLoader
+
+class Trainset(torch.utils.data.Dataset):
+  def __init__(self, list_IDs):
+        'Initialization'
+        self.list_IDs = list_IDs
+
+  def __len__(self):
+        'Denotes the total number of samples'
+        return len(self.list_IDs)
+
+  def __getitem__(self, index):
+        'Generates one sample of data'
+        # Select sample
+        ID = self.list_IDs[index]
+
+        # Load data and get label
+        X = X_train[ID]
+        y = Y_train[ID]
+        return X,y
+
+class Testset(torch.utils.data.Dataset):
+  def __init__(self, list_IDs):
+        'Initialization'
+        self.list_IDs = list_IDs
+
+  def __len__(self):
+        'Denotes the total number of samples'
+        return len(self.list_IDs)
+
+  def __getitem__(self,index):
+        'Generates one sample of data'
+        # Select sample
+        ID = self.list_IDs[index]
+
+        # Load data and get label
+        X = X_test[ID]
+        y = Y_test[ID]
+        return X,y
+
+class Validationset(torch.utils.data.Dataset):
+  def __init__(self, list_IDs):
+        'Initialization'
+        self.list_IDs = list_IDs
+
+  def __len__(self):
+        'Denotes the total number of samples'
+        return len(self.list_IDs)
+
+  def __getitem__(self, index):
+        'Generates one sample of data'
+        # Select sample
+        ID = self.list_IDs[index]
+
+        # Load data and get label
+        X = X_val[ID]
+        y = Y_val[ID]
+        return X,y
